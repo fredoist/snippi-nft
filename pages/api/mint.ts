dea8c8beff6c6661ba8b03147b5847de5569f1d6
fredoist <7684330+fredoist@users.noreply.github.com>
2022-01-31 23:10:18 -0600

    fix: remove buffer as it is already integrated on runtime

--- pages/api/mint.ts
+++ pages/api/mint.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Buffer } from 'buffer';
+// import { Buffer } from 'buffer';
 import { nftCollection } from '@utils/thirdweb';
 import nodeHtmlToImage from 'node-html-to-image';
 import { generateSnippet } from '@utils/generateSnippet';
@@ -18,7 +18,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const snippet = generateSnippet(name, code);
 
-  const file = Buffer.from(snippet, 'utf-8');
+  const file = Buffer.from(snippet);
   const image = await nodeHtmlToImage({
     html: snippet,
   });
