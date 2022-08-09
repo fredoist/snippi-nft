8fa8832d05449739a943172745e0eb5c544308a2
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-08-09 13:57:15 -0500

    Add custom document with styles on body

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html lang="en-US">
+    <Head />
+    <body className="bg-white font-sans leading-tight text-black antialiased [font-feature-settings:'cv11','cv10','ss01']">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
