2d8cb9be6dadc1b67220179bcf5c1b7a6fff2f1c
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-08-09 13:54:37 -0500

    Replace inter font package and remove unused ones

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from 'next/app';
 import { ThirdwebProvider, ChainId } from '@thirdweb-dev/react';
+
+import 'inter-ui';
 import 'tailwindcss/tailwind.css';
-import '@fontsource/montserrat/700.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/700.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', 'sans-serif'],
-        serif: ['Montserrat', 'serif'],
-      },
-    },
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    }
   },
   plugins: [],
 };
