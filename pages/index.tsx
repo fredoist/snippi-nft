bfe6fedbb3854183bc7afb6a74ad983133edd517
Freddy González <7684330+fredoist@users.noreply.github.com>
2022-08-10 01:14:43 -0500

    Lazy load collection items on scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { CodeIcon, ViewGridIcon } from '@heroicons/react/outline'
 import { collection } from '@utils/thirdweb'
 import { NFTCard } from '@components/NFTCard'
 import { NFTMetadata } from '@components/NFTCard'
+import { useNFTCollection } from '@thirdweb-dev/react'
 
 /**
  * @todo: replace for a remote origin
@@ -17,11 +18,14 @@ const NFTS = [
   'https://gateway.ipfscdn.io/ipfs/QmTfykVZu1yDKnpGuvZenUsXtY2bCSuHB9AE3DoJeXBMKj/0'
 ]
 
-const IndexPage: NextPage<{ nfts: NFTMetadata[] }> = ({ nfts }) => {
+const IndexPage: NextPage<{ data: NFTMetadata[] }> = ({ data }) => {
   const headerRef = React.useRef<HTMLDivElement | null>(null)
   const collectionRef = React.useRef<HTMLDivElement | null>(null)
   const [headerHeight, setHeaderHeight] = React.useState(0)
   const randomNFT = NFTS[Math.floor(Math.random() * NFTS.length)]
+  const collection = useNFTCollection(process.env.NEXT_PUBLIC_COLLECTION_ADDRESS)
+  const [nfts, setNFTs] = React.useState<NFTMetadata[]>(data)
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
     if (headerRef.current) {
@@ -37,38 +41,60 @@ const IndexPage: NextPage<{ nfts: NFTMetadata[] }> = ({ nfts }) => {
     }
   }, [collectionRef])
 
+  React.useEffect(() => {
+    const handleScroll = async () => {
+      // when scroll reach end of page load more nfts from client
+      if ((window.scrollY + window.innerHeight)  >= document.body.offsetHeight - 500) {
+        setIsLoading(true)
+        const data = (await collection?.getAll({
+          count: 3,
+          start: nfts.length + 1
+        })) as NFTMetadata[]
+        if (data) {
+          setNFTs([...nfts, ...data])
+          setIsLoading(false)
+        } else {
+          setIsLoading(false)
+          return
+        }
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [nfts, collection])
+
   return (
     <React.Fragment>
       <SEO
-        title="Interactive NFTs of frontend code snippets"
+        title="Turn your code snippets into non-fungible tokens"
         description="A collection of interactive NFTs of frontend code snippets that render in your browser"
       />
       <Navbar headerHeight={headerHeight} />
       <div ref={headerRef} className="relative bg-black px-5 py-24 md:py-32 lg:py-48">
-        <h1 className="relative z-10 mx-auto mb-8 md:mb-12 max-w-4xl text-center text-2xl sm:text-3xl font-bold text-white mix-blend-difference md:text-5xl lg:text-6xl">
+        <h1 className="relative z-10 mx-auto mb-8 max-w-4xl text-center text-2xl font-bold text-white mix-blend-difference sm:text-3xl md:mb-12 md:text-5xl lg:text-6xl">
           Turn your code snippets into non-fungible tokens
         </h1>
         <div className="relative z-10">
-          <div className="mx-auto flex flex-col md:flex-row max-w-max items-center gap-4">
+          <div className="mx-auto flex max-w-max flex-col items-center gap-4 md:flex-row">
             <Link href="/collection">
-              <a className="relative z-10 w-full whitespace-nowrap group inline-flex items-center justify-between gap-2 rounded-lg bg-white/40 p-4 font-medium text-white ring-1 ring-white/50 transition-colors hover:bg-white/50 hover:ring-white/80 backdrop-blur">
+              <a className="group relative z-10 inline-flex w-full items-center justify-between gap-2 whitespace-nowrap rounded-lg bg-white/40 p-4 font-medium text-white ring-1 ring-white/50 backdrop-blur transition-colors hover:bg-white/50 hover:ring-white/80">
                 <ViewGridIcon className="h-5 w-5 transition-transform duration-100 ease-in-out group-hover:-rotate-45 group-hover:scale-110" />
                 <span>Explore Collection</span>
               </a>
             </Link>
             <Link href="/new">
-              <a className="relative z-10 w-full whitespace-nowrap group inline-flex items-center justify-between gap-2 rounded-lg bg-white/40 p-4 font-medium text-white ring-1 ring-white/50 transition-colors hover:bg-white/50 hover:ring-white/80 backdrop-blur">
+              <a className="group relative z-10 inline-flex w-full items-center justify-between gap-2 whitespace-nowrap rounded-lg bg-white/40 p-4 font-medium text-white ring-1 ring-white/50 backdrop-blur transition-colors hover:bg-white/50 hover:ring-white/80">
                 <CodeIcon className="h-5 w-5 transition-transform duration-100 ease-in-out group-hover:-rotate-45 group-hover:scale-110" />
                 <span>Create Your Own</span>
               </a>
             </Link>
           </div>
         </div>
-        <div className="absolute inset-0 overflow-hidden z-0">
+        <div className="absolute inset-0 z-0 overflow-hidden">
           <iframe
             className="h-full w-full overflow-hidden object-cover"
             src={randomNFT}
-            loading="eager"
+            loading="lazy"
             scrolling="no"
           />
         </div>
@@ -76,6 +102,10 @@ const IndexPage: NextPage<{ nfts: NFTMetadata[] }> = ({ nfts }) => {
       <main ref={collectionRef} role="main" className="group px-4 pt-12">
         <div className="mx-auto grid max-w-7xl gap-6 pb-24 pt-12 lg:grid-cols-3">
           {nfts.length > 0 && nfts.map((nft, index) => <NFTCard key={index} nft={nft} />)}
+          {isLoading &&
+            Array.from({ length: 3 }).map((_, key) => (
+              <div key={key} className="aspect-square animate-pulse rounded-xl bg-neutral-300" />
+            ))}
         </div>
       </main>
     </React.Fragment>
@@ -83,15 +113,15 @@ const IndexPage: NextPage<{ nfts: NFTMetadata[] }> = ({ nfts }) => {
 }
 
 export const getStaticProps: GetStaticProps<{
-  nfts: NFTMetadata[]
+  data: NFTMetadata[]
 }> = async () => {
   try {
-    const nfts = await collection.getAll()
+    const nfts = await collection.getAll({ count: 3, start: 0 })
     const data = JSON.parse(JSON.stringify(nfts))
-    return { props: { nfts: data } }
+    return { props: { data } }
   } catch (error) {
     console.error(error)
-    return { props: { nfts: [] } }
+    return { props: { data: [] } }
   }
 }
 
